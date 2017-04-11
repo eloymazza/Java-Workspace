1b74ca9620dc6e118080fd7f0648ded4297d4653
Eloy Mazza <eloymazza.ra@gmail.com>
2017-04-11 03:12:20 -0300

    avance con grafos

--- Progra3TP3/src/ejercicio1/Graff.java
+++ Progra3TP3/src/ejercicio1/Graff.java
@@ -1,5 +1,4 @@
-package ejercicio1;
-
+package ejercicio1;;
 public class Graff {
 	
 	NodeList[] graff;
@@ -10,6 +9,14 @@ public class Graff {
 		graff = new NodeList[n];
 		size = 0;
 		
+	}
+	
+	public boolean isEmpty(){
+		return size == 0;
+	}
+	
+	public int getSize(){
+		return size;
 	}
 	
 	public void addAdjacent(int origin, int destination){
@@ -23,26 +30,19 @@ public class Graff {
 		}
 	}
 	
-	public boolean isEmpty(){
-		return size == 0;
-	}
-	
-	public int getSize(){
-		return size;
-	}
-	
 	public  void graffGenerator(int n){
 		
 		int a = n;
-		for (int i = 0; i < graff.length-n; i++) {			
+		int length = graff.length;
+		int specialLenght = length-n-1;
+		for (int i = 0; i < length; i++) {			
 			for (int j = i+1; j <= a; j++) {
 				addAdjacent(i, j);
-				
 			}
-			a++;
-		}
-		addAdjacent(graff.length-n, graff.length-1);
-		
+			if(i < specialLenght){
+				a++;				
+			}
+		}	
 	}
 	public String toString(){
 		
@@ -56,13 +56,10 @@ public class Graff {
 		return null;
 	}
 	
-	
 	public static void main(String[] args) {
 		
-		Graff g = new Graff(10);
-				
-		g.graffGenerator(2);
-		
+		Graff g = new Graff(20);
+		g.graffGenerator(4);
 		System.out.println(g.toString());
 
 	}
