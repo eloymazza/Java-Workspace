cce14eec3b5432fc52e76fb9d21db83ad940ed36
Eloy Mazza <eloymazza.ra@gmail.com>
2017-04-25 18:15:20 -0300

    Avance en el ejercicio de la mochila

--- Progra3TP4/src/ejercicio2/MarketAssault.java
+++ Progra3TP4/src/ejercicio2/MarketAssault.java
@@ -9,8 +9,13 @@ public class MarketAssault {
 	private static List<Item> startItems;
 	private static Map<String, Double> solution;
 	private static double solutionValue;
-	public static boolean isFractal = false;
+ 	public static boolean isFractal = false;
 
+	
+	/*
+	 * Este metodo recibe un conjunto de items, un peso maximo que puede cargar la mochila, y 
+	 * un booelano que indica si los items del conjunto dado son fraccionables.
+	 */
 	public static Map<String, Double> fillBag(Set<Item> items, Double weight, boolean isFractal){
 
 		setStartingTools(weight); // inicializo las estructuras y variables que necesito para esta funcion 
@@ -28,21 +33,22 @@ public class MarketAssault {
 			startItems.remove(bestPick); // Elimino el item que elegí como mejor opcion.
 			if(isFractal){
 				// Calculo la mayor cantidad en kg del mejor candidato que puede entrar en la mochila.
-				solution.put(bestPick.getName(), bestPickQuantity); // Agrego a mi solucion el nombre del item mas la proporcion del mismo que pongo en la mochila
 				bestPickQuantity = getQuantityInKG(bestPick)/bestPick.getUnitaryWeight(); 
+				solution.put(bestPick.getName(), bestPickQuantity); // Agrego a mi solucion el nombre del item mas la proporcion del mismo que pongo en la mochila
 			}
 			else{
-				// Calculo la cantidad de items del mejor candidato que puden entrar en la mochila. 
+				// Calculo la cantidad de items (sin fraccionar) del mejor candidato que puden entrar en la mochila. 
 				bestPickQuantity = getQuantity(bestPick);
-				if(bestPickQuantity != 0){
+				if(bestPickQuantity != 0){ // Solo agrego el item si la canidad que entra es mayor a 0.
 					solution.put(bestPick.getName(), bestPickQuantity);
 				}
 			}
 			currentWeight += bestPickQuantity * bestPick.getUnitaryWeight(); // Actualizo el peso actual que tiene mi mochila.
 			solutionValue += bestPick.getUnitaryPrice()* bestPickQuantity; // Actualizo total en valor de los items de mi mochila.
 		}
-		return solution;
+		return solution;  
 	}
+
 	
 	static void setStartingTools(double weight){
 
@@ -54,39 +60,50 @@ public class MarketAssault {
 
 	}
 	
+	// Retorna la cantidad (no fraccional) del item elegido que puede ser ingresado en la mochila sin sobrepasar su peso restante.
 	private static double getQuantity(Item bestPick) {
 		
-		double capacityLeft = bagCapacity - currentWeight;
-		double quantity = bestPick.getQuantity();
+		double capacityLeft = bagCapacity - currentWeight;  // Capacidad que le queda a la mochila
+		double quantity = bestPick.getQuantity(); 
+		// Mientras el total de existencias del item elegido no entre en la mochila, se le resta una existencia.
 		while(capacityLeft < bestPick.getUnitaryWeight()* quantity && quantity != 0){
-			quantity--;
+			quantity--;  
 		}
 		return quantity;
 	}
+	
+	// REtorna la cantidad fraccional del item elegido que puede ser ingresado en la mochila sin sobrepasar su peso restante.   
+	private static Double getQuantityInKG(Item bestPick) {
 
-	static Double getQuantityInKG(Item bestPick) {
-
-		double capacityLeft = bagCapacity - currentWeight;	
+		double capacityLeft = bagCapacity - currentWeight;
+		/* Si la capacidad restante de la mochila es mayor o igual al peso del item elegido 
+		multiplicado por su cantidad, retorno este ultimo valor, es decir, pongo toda la cantidad
+		que exista del item elegido en la mochila.
+		De lo contrario, fracciono el item para llenar lo que quede de la mochila. 
+		*/ 
 		if(capacityLeft >= bestPick.getUnitaryWeight()*bestPick.getQuantity()){
 			return bestPick.getUnitaryWeight()*bestPick.getQuantity();
 		}
 		return capacityLeft;
 
 	}
-
+	
+	// Siempre retorno el primer item, ya que la lista de items esta ordenada de acuerdo a la mejor relacion precio/peso.
 	private static Item selection() {
 		return startItems.get(0);
 	}
 
+	// Retorno true si el peso total de la mochila fue alcanzado.
 	private static boolean solution() {
-		// TODO Auto-generated method stub
-		return currentWeight >=  bagCapacity;
+		return currentWeight ==  bagCapacity;
 	}
-
+	
+	// Retorna la sumatoria de los precios de los productos que fueron ingresados en la mochila.
 	private static double getSolutionValue() {
 		return solutionValue;
 	}
 
+	// Ordena los items con la mayor relacion precio/peso.
 	private static void sortItems(Set<Item> items){
 		Iterator<Item> it = items.iterator();
 		while(it.hasNext()){
